35c860ecf6f4606236e9b8523d56192445615baf
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-09-28 20:51:11 -0300

    Criado interface básica

---
+++ src/components/Module.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Module() {
+    return (
+        <>
+            <div className="Module">
+                <div className="Module__progress"></div>
+                <div className="Module__info"></div>
+            </div>
+        </>
+    )
+}
+
+export default Module;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import App from './App';
+import Module from './Module';
+
+export { App, Module };
