de6cf727b4871208cbf3586e9215a39c897c4269
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-10-08 21:25:36 -0300

    Melhora no código

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import CoursePage from './CoursePage';
+import CourseHolder from './CourseHolder';
+import Course from './Course';
 import ModuleContainer from './ModuleContainer';
 import ModulePage from './ModulePage'
 import Module from './Module';
 
-export { CoursePage, ModuleContainer, ModulePage, Module };
+export { CourseHolder, Course, CoursePage, ModuleContainer, ModulePage, Module };
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     },
 ]);
 
-const Routes = () => {
+function Routes() {
    return(
        <RouterProvider router={router}/>
    )
