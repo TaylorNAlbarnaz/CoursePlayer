5f78a131b802b1a1c034a1fc97f7f546e6d665b5
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-10-08 21:45:03 -0300

    Adicionado importador de módulo

--- src/components/ModuleContainer.js
+++ src/components/ModuleContainer.js
@@ -6,6 +6,8 @@ function ModuleContainer(props) {
         <>
             <div className="Container__module workspace">
                 <h1>Módulos</h1>
+                <label htmlFor="arquivo" style={{color: "var(--colors-ltgray)"}}>Importar módulos</label>
+                <input type="file" id="arquivo" style={{display: "none"}}/>
                 <div className="Modules">
                     {props.children}
                 </div>
