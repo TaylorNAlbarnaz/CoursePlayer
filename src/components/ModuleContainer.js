b1d41ae9bf3a47fac3a4a46c5d16d572b54c983f
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-10-10 22:43:38 -0300

    Adicionado upload de arquivo

--- src/components/ModuleContainer.js
+++ src/components/ModuleContainer.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/ModuleContainer.css";
 import "../styles/index.css";
 
 function ModuleContainer(props) {
+    const [file, setFile] = useState(null);
+
     return (
         <>
             <div className="Container__module workspace">
                 <h1>Módulos</h1>
-                <label htmlFor="arquivo" style={{color: "var(--colors-ltgray)"}}>Importar módulos</label>
-                <input type="file" id="arquivo" style={{display: "none"}}/>
+                <label 
+                    htmlFor="arquivo" 
+                    style={
+                        file ?
+                        {color: "var(--colors-green)"}
+                        :
+                        {color: "var(--colors-ltgray)"}
+                    }
+                >Importar módulos</label>
+                <input type="file" id="arquivo" style={{display: "none"}} onChange={(e)=> {setFile(e.target.files[0])}}/>
                 <div className="Modules">
                     {props.children}
                 </div>
